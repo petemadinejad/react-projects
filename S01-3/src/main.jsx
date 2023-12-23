73a9b271da25775264b5a6a92e5646618afd0ee2
petemadinejad <parisaetemadinejad.job@gmail.com>
2023-12-23 12:20:30 +0330

    create a react project and initialize that

---
+++ S01-3/src/main.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom/client";
+
+function App(){
+    return <h1>Hi react gangs</h1>
+}
+
+const root = document.getElementById("root")
+ReactDOM.createRoot(root).render(<App/>)
+
